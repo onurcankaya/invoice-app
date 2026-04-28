8071f4720c184479b6bcb730277143d06914ce3b
Onur <onur.eren.cankaya@gmail.com>
2026-04-28 14:44:18 +0200

    initial project setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <section id="center">
+        <div>
+          <h3>Invoice App</h3>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,36 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: 'class',
+   theme: {
+    extend: {
+      colors: {
+        purple: {
+          DEFAULT: '#7C5DFA',
+          light: '#9277FF',
+        },
+        navy: {
+          DEFAULT: '#1E2139',
+          light: '#252945',
+        },
+        slate: {
+          light: '#DFE3FA',
+          DEFAULT: '#888EB0',
+          dark: '#7E88C3',
+        },
+        dark: {
+          DEFAULT: '#0C0E16',
+          light: '#141625',
+        },
+        red: {
+          DEFAULT: '#EC5757',
+          light: '#FF9797',
+        },
+        bg: '#F8F8FB',
+      }
+    },
+  },
+  plugins: [],
+}
